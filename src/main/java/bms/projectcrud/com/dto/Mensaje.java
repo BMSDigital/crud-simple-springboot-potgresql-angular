097f7be24ee20bf431f7234ed9b3d4ce8df5bf88
Fraibert Bracho <fraiberth1992@gmail.com>
2022-03-21 00:18:15 -0400

    Commit crud completo de la app

--- src/main/java/bms/projectcrud/com/dto/Mensaje.java
+++ src/main/java/bms/projectcrud/com/dto/Mensaje.java
@@ -1,4 +1,17 @@
 package bms.projectcrud.com.dto;
 
 public class Mensaje {
+    private String Mensaje;
+
+    public Mensaje(String mensaje) {
+        Mensaje = mensaje;
+    }
+
+    public String getMensaje() {
+        return Mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        Mensaje = mensaje;
+    }
 }
--- src/main/java/bms/projectcrud/com/entity/Producto.java
+++ src/main/java/bms/projectcrud/com/entity/Producto.java
@@ -1,4 +1,48 @@
 package bms.projectcrud.com.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "producto")
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+    @Column(name = "precio", nullable = false)
+    private Float precio;
+
+    public Producto() {
+
+    }
+
+    public Producto(String nombre, Float precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Float precio) {
+        this.precio = precio;
+    }
 }
